c56f095f847a71e731f8022a0b95291e55cf080c
Le Van Huy <huylv1@itsol.vn>
2018-12-24 17:27:25 +0700

    add webpack config

---
+++ log.js
@@ -0,0 +1,3 @@
+export default (string) => {
+    console.log(string);
+}
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './bundle.js'        
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
